1e8c0b5f5474142883c71612880720c928d80ca6
Tom <tom-vautray@hotmail.fr>
2022-11-05 01:06:56 +0100

    Create unbound modules

---
+++ src/app/vue/vue.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StarChartComponent } from './star-chart/star-chart.component';
+
+
+
+@NgModule({
+  declarations: [
+    StarChartComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class VueModule { }
