ac0fb983cdad60f9b73737dda55e5523f0b48fae
Tom <tom-vautray@hotmail.fr>
2022-11-13 19:54:28 +0100

    Unwrap sol-star-chart from its sub-sub folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SolStarChartComponent } from './view/sol-star-chart/sol-star-chart/sol-star-chart.component';
+import { SolStarChartComponent } from './view/sol-star-chart/sol-star-chart.component';
 
 const routes: Routes = [
   {
--- src/app/view/view.module.ts
+++ src/app/view/view.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { FeatureModule } from '../feature/feature.module';
-import { SolStarChartComponent } from './sol-star-chart/sol-star-chart/sol-star-chart.component';
+import { SolStarChartComponent } from './sol-star-chart/sol-star-chart.component';
 
 
 
