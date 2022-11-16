9fd02811248cfcc60b992f6891a4156c42a04012
Tom <tom-vautray@hotmail.fr>
2022-11-16 19:58:29 +0100

    Create node-model/cloneDefaultValue

--- src/app/model/node-model.ts
+++ src/app/model/node-model.ts
@@ -1,7 +1,13 @@
+import { cloneObject } from "../utils/data-manipulation";
+
 export interface NodeModel {
     name: string
 }
 
 export const defaultValue: Readonly<NodeModel> = {
     name: ""
+}
+
+export function cloneDefaultValue(): Readonly<NodeModel> {
+    return cloneObject<Readonly<NodeModel>>(defaultValue);
 }
