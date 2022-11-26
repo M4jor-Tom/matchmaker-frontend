d39d0f4bdb827c7acaf77c82dc83cc7e8761cf8e
Tom <tom-vautray@hotmail.fr>
2022-11-26 18:46:21 +0100

    Connect NodeComponent to NodeDataService

--- src/app/base/node/node.component.ts
+++ src/app/base/node/node.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as nm from 'src/app/logic/model/interface/node-model';
+import { NodeDataService } from './node-data/node-data.service';
 
 @Component({
   selector: 'app-node',
   templateUrl: './node.component.html',
   styleUrls: ['./node.component.sass']
 })
-export class NodeComponent implements OnInit {
+export class NodeComponent implements AfterViewInit {
 
-  constructor() { }
+  private nodeModel: nm.NodeModel;
 
-  ngOnInit(): void {
+  constructor(private nodeDataService: NodeDataService) {
+    this.nodeModel = nm.cloneDefaultValue();
   }
 
+  public ngAfterViewInit(): void {
+    this.initDynamics();
+  }
+
+  private initDynamics(): void {
+    this.nodeDataService.getObservable.subscribe(props => {
+      this.nodeModel = nm.updateWithDynamicProperties(this.nodeModel, props);
+    });
+  }
 }
