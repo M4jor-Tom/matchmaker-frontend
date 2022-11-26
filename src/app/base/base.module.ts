78960f22d73a2dab041f735a77311359bea697da
Tom <tom-vautray@hotmail.fr>
2022-11-26 18:12:59 +0100

    ng g c base/node

--- src/app/base/base.module.ts
+++ src/app/base/base.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { NgCoreModule } from '../ng-core/ng-core.module';
 import { CelestialTemplateComponent } from './celestial-template/celestial-template.component';
+import { NodeComponent } from './node/node.component';
 
 
 
 @NgModule({
   declarations: [
     CelestialTemplateComponent,
+    NodeComponent,
   ],
   imports: [
     NgCoreModule
