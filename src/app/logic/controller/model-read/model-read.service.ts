7726be5945477739ce2894437e3526ac04d92b66
Tom <tom-vautray@hotmail.fr>
2022-11-26 19:19:11 +0100

    model-read-service subscribes and unsuscribes to/from nodes

--- src/app/logic/controller/model-read/model-read.service.ts
+++ src/app/logic/controller/model-read/model-read.service.ts
@@ -3,6 +3,8 @@ import { Subject } from 'rxjs';
 import { CompatClient, Message, Stomp } from "@stomp/stompjs"
 import * as SockJS from 'sockjs-client';
 import { NodeModel } from 'src/app/logic/model/interface/node-model';
+import { NodeId } from '../../model/enum/node-id';
+import { NodeSubscription } from '../../model/interface/request-type/node-subscription';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +12,10 @@ import { NodeModel } from 'src/app/logic/model/interface/node-model';
 export class ModelReadService {
 
   private static WEBSOCKET_ENDPOINT_URL: Readonly<string> = "http://localhost:8081/ws-endpoint";
+
+  private static WEBSOCKET_BROKER_URL: Readonly<string> = "http://localhost:8081/ws-broker";
+
+  private static WEBSOCKET_DESTINATION_URL: Readonly<string> = "http://localhost:8081/ws-destination";
 
   private stompClient: CompatClient;
 
@@ -26,16 +32,34 @@ export class ModelReadService {
 
   }
 
-  public wsConnect(brokerUrl: string): void {
+  public wsConnect(): void {
     const self: ModelReadService = this;
     this.stompClient.configure({
       onConnect: function(frame: any) {
-        self.stompClient.subscribe(brokerUrl, function(message: Message) {
+        self.stompClient.subscribe(ModelReadService.WEBSOCKET_BROKER_URL, function(message: Message) {
           self.subject.next(JSON.parse(ModelReadService.decodeMessage(message)));
         });
       }
     });
     this.stompClient.activate();
+  }
+
+  public subscribeToNodeOfId(nodeId: NodeId): void {
+    this.sendNodeSubscription({
+      nodeId: nodeId,
+      ifTrueAddElseRemove: true
+    });
+  }
+
+  public unsubscribeFromNodeOfId(nodeId: NodeId): void {
+    this.sendNodeSubscription({
+      nodeId: nodeId,
+      ifTrueAddElseRemove: false
+    });
+  }
+
+  private sendNodeSubscription(nodeSubscription: NodeSubscription): void {
+    this.stompClient.send(ModelReadService.WEBSOCKET_DESTINATION_URL, {}, JSON.stringify(nodeSubscription));
   }
 
   public get getNodeModelSubject(): Subject<NodeModel> {
