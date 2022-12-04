d5c2aa0d56e9416419b6ff395dfa544cc4734e63
Tom <tom-vautray@hotmail.fr>
2022-12-04 23:05:30 +0100

    node-database now is a map with NodeId keys

--- src/app/logic/database/node-database.ts
+++ src/app/logic/database/node-database.ts
@@ -1,20 +1,23 @@
-import { cloneObject } from "src/app/utils/data-manipulation";
 import { NodeId } from "../model/enum/node-id";
 import * as nm from "../model/interface/node-model";
 
-export const nodeDatabase: nm.NodeModel[] = cloneObject<nm.NodeModel[]>([
-    nm.newNode(NodeId.MARIANA),
-    nm.newNode(NodeId.E_PRIME),
-    nm.newNode(NodeId.LITH),
-    nm.newNode(NodeId.ERPO),
-    nm.newNode(NodeId.EVEREST),
-    nm.newNode(NodeId.GAIA),
-    nm.newNode(NodeId.MANTLE),
-    nm.newNode(NodeId.CAMBRIA),
-    nm.newNode(NodeId.EURASIA),
-    nm.newNode(NodeId.PACIFIC),
-    nm.newNode(NodeId.CERVANTES),
-    nm.newNode(NodeId.COBA),
-    nm.newNode(NodeId.TIKAL),
-    nm.newNode(NodeId.ORO)
-]);
+export function getNodeDatabase(): Map<NodeId, nm.NodeModel> {
+    const map: Map<NodeId, nm.NodeModel> = new Map<NodeId, nm.NodeModel>();
+
+    map.set(NodeId.MARIANA, nm.newNode(NodeId.MARIANA));
+    map.set(NodeId.E_PRIME, nm.newNode(NodeId.E_PRIME));
+    map.set(NodeId.LITH, nm.newNode(NodeId.LITH));
+    map.set(NodeId.ERPO, nm.newNode(NodeId.ERPO));
+    map.set(NodeId.EVEREST, nm.newNode(NodeId.EVEREST));
+    map.set(NodeId.GAIA, nm.newNode(NodeId.GAIA));
+    map.set(NodeId.MANTLE, nm.newNode(NodeId.MANTLE));
+    map.set(NodeId.CAMBRIA, nm.newNode(NodeId.CAMBRIA));
+    map.set(NodeId.EURASIA, nm.newNode(NodeId.EURASIA));
+    map.set(NodeId.PACIFIC, nm.newNode(NodeId.PACIFIC));
+    map.set(NodeId.CERVANTES, nm.newNode(NodeId.CERVANTES));
+    map.set(NodeId.COBA, nm.newNode(NodeId.COBA));
+    map.set(NodeId.TIKAL, nm.newNode(NodeId.TIKAL));
+    map.set(NodeId.ORO, nm.newNode(NodeId.ORO));
+
+    return map;
+}
