97615331111744b30b5f84a736472dc5d02fd417
Tom <tom-vautray@hotmail.fr>
2022-12-04 20:39:08 +0100

    Fix websocket

--- src/app/logic/controller/websocket/websocket.service.ts
+++ src/app/logic/controller/websocket/websocket.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-import { CompatClient, Message, Stomp } from "@stomp/stompjs"
+import { ActivationState, Client, CompatClient, IFrame, IMessage, Message, Stomp } from "@stomp/stompjs"
 import * as SockJS from 'sockjs-client';
 import { NodeModel } from 'src/app/logic/model/interface/node-model';
 import { NodeId } from '../../model/enum/node-id';
@@ -13,9 +13,9 @@ export class WebsocketService {
 
   private static ENDPOINT_URL: Readonly<string> = "http://localhost:8081/ws-endpoint";
 
-  private static BROKER_URL: Readonly<string> = "http://localhost:8081/ws-broker";
+  private static BROKER_URL: Readonly<string> = "/ws-broker";
 
-  private static DESTINATION_URL: Readonly<string> = "http://localhost:8081/ws-destination";
+  private static DESTINATION_URL: Readonly<string> = "/ws-destination";
 
   private stompClient: CompatClient;
 
@@ -28,8 +28,7 @@ export class WebsocketService {
       return new SockJS(WebsocketService.ENDPOINT_URL);
     });
 
-    this.subject = new Subject<NodeModel>();
-
+    this.subject = new Subject<NodeModel[]>();
   }
 
   //  USER ACCESSIBLE FUNCTIONALITIES  --  START
@@ -60,13 +59,51 @@ export class WebsocketService {
   public wsConnect(): void {
     const self: WebsocketService = this;
     this.stompClient.configure({
-      onConnect: function(frame: any) {
+      onConnect: function(frame: IFrame) {
+        // frame.headers = {"server": "matchmaker-backend"};
+        console.log("onConnect: " + frame.body);
         self.stompClient.subscribe(WebsocketService.BROKER_URL, function(message: Message) {
           self.subject.next(JSON.parse(WebsocketService.decodeMessage(message)));
         });
+      },
+
+      onChangeState: function(state: ActivationState) {
+        console.log("onChangeState: " + state.toString());
+      },
+
+      onStompError: function(frame: IFrame) {
+        console.error("onStompError: " + frame.body);
+      },
+
+      onWebSocketError: function(evt: any) {
+        console.error("WebSocketError");
+      },
+
+      onWebSocketClose: function(evt: any) {
+        console.log("WebSocketClose");
+      },
+
+      onDisconnect: function(receipt: IFrame) {
+        console.log("onDisconnect: " + receipt.body);
+      },
+
+      onUnhandledFrame: function(receipt: IFrame) {
+        console.warn("onUnhandledFrame: " + receipt.body)
+      },
+
+      onUnhandledMessage: function(message: IMessage) {
+        console.warn("onUnhandledMessage: " + message.body)
+      },
+
+      onUnhandledReceipt: function(receipt: IFrame) {
+        console.warn("onUnhandledReceipt: " + receipt.body);
       }
     });
     this.stompClient.activate();
+  }
+
+  public wsDisconnect() {
+    this.stompClient.disconnect();
   }
 
   private sendNodeSubscription(nodeSubscription: NodeSubscription): void {
