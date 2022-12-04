621581567ddc1b203f18dfdfe144a48cd0b26422
Tom <tom-vautray@hotmail.fr>
2022-12-04 20:41:51 +0100

    Init ws of node-list in constructor

--- src/app/feature/node-list/node-list.component.ts
+++ src/app/feature/node-list/node-list.component.ts
@@ -1,30 +1,28 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { NodeDataService } from 'src/app/base/node/node-data/node-data.service';
 import { WebsocketService } from 'src/app/logic/controller/websocket/websocket.service';
 import { NodeId, nodeIdToBaseId } from 'src/app/logic/model/enum/node-id';
 import { NodeModel } from 'src/app/logic/model/interface/node-model';
+import { getElementByIdOrThrow } from 'src/app/utils/html-utils';
 
 @Component({
   selector: 'app-node-list',
   templateUrl: './node-list.component.html',
   styleUrls: ['./node-list.component.sass']
 })
-export class NodeListComponent implements AfterViewInit {
+export class NodeListComponent implements OnDestroy {
+
+  private subscription: Subscription;
 
   constructor(
     private websocketService: WebsocketService,
     private nodeDataService: NodeDataService
   ) {
-    this.websocketService.wsConnect();
-  }
-
-  ngAfterViewInit(): void {
-    this.initDataTransfert();
-  }
-
-  private initDataTransfert(): void {
     const self: NodeListComponent = this;
-    this.websocketService.getNodeModelSubjects.subscribe(nodeModel => {
+
+    this.websocketService.wsConnect();
+    this.subscription = this.websocketService.getNodeModelSubjects.subscribe(nodeModel => {
       nodeModel.forEach(function(nodeModel: NodeModel) {
         self.nodeDataService.setProperties = {
           baseId: nodeIdToBaseId(nodeModel.nodeId),
@@ -33,5 +31,10 @@ export class NodeListComponent implements AfterViewInit {
         }
       });
     });
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+    this.websocketService.wsDisconnect();
   }
 }
